b1918054a39ad8c59b2f6a7b0b0b2f8ace44e1a0
Daniel Weber <pennwe@gmail.com>
2015-06-14 16:46:53 -0400

    moved eventlist class to its own file

--- Unity/Assets/Scripts/EventInterface.cs
+++ Unity/Assets/Scripts/EventInterface.cs
@@ -6,31 +6,3 @@ interface EventInterface {
 	void eventExecute(PlayerShip ship);
 	float chanceToHappen(PlayerShip ship);
 }
-
-static class EventList {
-	public static List<EventInterface> allEvents;
-
-	static EventList() {
-		allEvents = new List<EventInterface>();
-		allEvents.Add(new NullEvent());
-	}
-
-	public static void eventGenerator(PlayerShip ship) {
-		float chancesum = 0.0f;
-		int eventindex = 0;
-		List<EventInterface> candidateEvents = new List<EventInterface> ();
-		foreach (EventInterface cur in allEvents) {
-			float curchance = cur.chanceToHappen(ship);
-			if (curchance > 0) {
-				chancesum += curchance;
-				candidateEvents.Add(cur);
-			}
-		}
-		float chance = Random.value * chancesum;
-		while (chance > 0 && eventindex < (candidateEvents.Count - 1)) {
-			chance -= candidateEvents[eventindex].chanceToHappen(ship);
-			eventindex ++;
-		}
-		candidateEvents [eventindex].eventExecute (ship);
-	}
-}
---
+++ Unity/Assets/Scripts/EventList.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public static class EventList{
+	public static List<EventInterface> allEvents;
+	
+	static EventList() {
+		allEvents = new List<EventInterface>();
+		allEvents.Add(new NullEvent());
+	}
+	
+	public static void eventGenerator(PlayerShip ship) {
+		float chancesum = 0.0f;
+		int eventindex = 0;
+		List<EventInterface> candidateEvents = new List<EventInterface> ();
+		foreach (EventInterface cur in allEvents) {
+			float curchance = cur.chanceToHappen(ship);
+			if (curchance > 0) {
+				chancesum += curchance;
+				candidateEvents.Add(cur);
+			}
+		}
+		float chance = Random.value * chancesum;
+		while (chance > 0 && eventindex < (candidateEvents.Count - 1)) {
+			chance -= candidateEvents[eventindex].chanceToHappen(ship);
+			eventindex ++;
+		}
+		candidateEvents [eventindex].eventExecute (ship);
+	}
+}
